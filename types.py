2529a1c1c8ee23f020a37fcb34d7f68356c9da9d
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-11-13 21:21:40 +0100

    More code

---
+++ types.py
@@ -0,0 +1,19 @@
+s = 'Welcome to this tutorial'
+print(s)
+
+print(type(s))
+
+print(s[1])
+print(s[2])
+print(s[-1])
+
+a = []
+a = [1, 2, 3]
+print(a)
+
+b = ["Anderson", "Test", 1, 2, 3]
+print(b)
+
+c = ["Test", "Software", "System"]
+print(c[0])
+print(c[2])
