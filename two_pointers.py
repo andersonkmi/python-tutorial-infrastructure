20af7341eeed6ff9c9540b3e0afb1cf1485a4111
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-11-24 21:19:12 +0100

    Add pair sum using 2 pointers

--- two_pointers.py
+++ two_pointers.py
@@ -5,3 +5,17 @@ def pair_sum_sorted_brute_force(nums, target):
             if nums[i] + nums[j] == target:
                 return [i, j]
     return []
+
+
+def pair_sum_sorted(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+    while left < right:
+        result = numbers[left] + numbers[right]
+        if result < target:
+            left += 1
+        elif result > target:
+            right -= 1
+        else:
+            return [left, right]
+    return []
