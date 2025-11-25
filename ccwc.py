6690a1c3ea3c952b8f4bd8c12abb1ad85dbe8f47
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-11-25 23:07:27 +0100

    More changes

--- ccwc.py
+++ ccwc.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+import sys
 
 def main():
-    print("Welcome to the Word counter utility")
+    if len(sys.argv) < 3:
+        print("Usage: ccwc.py <option> <file>")
+        print("Options: -c, -l, -w, -m")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
