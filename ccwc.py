5797b8811fd3e76427c0a2a46bc0ed654eb06bf4
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-11-25 23:33:14 +0100

    Add option selection

--- ccwc.py
+++ ccwc.py
@@ -7,5 +7,23 @@ def main():
         print("Options: -c, -l, -w, -m")
         sys.exit(1)
 
+    option = sys.argv[1]
+    file_name = sys.argv[2]
+
+    match option:
+        case "-c":
+            print("Option -c selected")
+        case "-l":
+            print("Option -l selected")
+        case "-w":
+            print("Option -w selected")
+        case "-m":
+            print("Option -m selected")
+        case _:
+            print("Option {} is invalid".format(option))
+            sys.exit(1)
+
+    sys.exit(0)
+
 if __name__ == "__main__":
     main()
